01ad22087bdc149b0ec7b33aa1eb126843dfedee
elolof24-glitch <elolof24@gmail.com>
2026-05-08 20:37:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
 
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Bot is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
